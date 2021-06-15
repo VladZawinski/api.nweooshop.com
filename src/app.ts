fb21ed9f19042ebebdc565db78b787679b0a28fc
nayyaung9 <naygit.developer@gmail.com>
2021-06-15 16:40:34 +0630

    build: node api project init

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import MongoStore from "connect-mongo";
+import path from "path";
+import mongoose from "mongoose";
+import bluebird from "bluebird";
+import { MONGODB_URI, SESSION_SECRET } from "./utils/secrets";
+
+// Create Express server
+const app = express();
+
+// Connect to MongoDB
+const mongoUrl = MONGODB_URI;
+mongoose.Promise = bluebird;
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+    );
+    // process.exit();
+  });
+
+// Express configuration
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+      mongoUrl,
+      mongoOptions: {
+        autoReconnect: true,
+      },
+    }),
+  })
+);
+app.use((req: any, res: any, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+export default app;
