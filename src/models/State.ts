8405d425008786a03fa6621442c17d3c44d04887
nayyaung9 <naygit.developer@gmail.com>
2021-06-22 19:50:08 +0630

    fix: fix state model

--- src/models/State.ts
+++ src/models/State.ts
@@ -14,14 +14,6 @@ const stateSchema = new mongoose.Schema<StateDocument>(
   { timestamps: true, _id: false }
 );
 
-stateSchema.set("toJSON", {
-  virtuals: true,
-  versionKey: false,
-  transform: function (doc: any, ret: any) {
-    delete ret._id;
-  },
-});
-
 const State = mongoose.model<StateDocument>("State", stateSchema);
 
 export default State;
